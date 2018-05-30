6bf2599869fc644a0c6dace9aed2b06e32300f17
Jenell <nellarro@gmail.com>
2018-05-29 21:16:51 -0400

    third time

--- address-book/client-react/src/App.js
+++ address-book/client-react/src/App.js
@@ -6,7 +6,15 @@ class App extends Component {
   componentDidMount() {
     fetch('api/oddballs').then(resp => resp.json()).then(data => {
       console.log(data)
+      this.setState({contacts: data})
     })
+  }
+
+  constructor() {
+    super()
+    this.state = {
+      contacts: []
+    }
   }
   render() {
     return (
@@ -15,28 +23,55 @@ class App extends Component {
             type="text"
             name="search"
           />
-          <ContactList />
+          <ContactList contacts={this.state.contacts} />
       </div>
     );
   }
 }
 
 class ContactList extends Component {
-  render(){
-    return [
-      <Contact />,
-      <Contact />,
-      <Contact />,
-      <Contact />
-    ]
+  constructor() {
+    super()
+    this.state = {
+      isActive: null
+    }
   }
-
+  clickAThing = key => {
+    console.warn('clicked!', key)
+    this.setState({ isActive: key })
+  }
+  render(){
+   const mapOverContacts = dataFromBack => {
+    return dataFromBack.map(contact => (
+      <Contact 
+        key={contact.id}
+        useableKey={contact.id}
+        firstName={contact.firstName}
+        lastName={contact.lastName}
+        street={contact.street}
+        city={contact.city}
+        state={contact.state}
+        email={contact.email}
+        onClick={this.clickAThing}
+        isActive={this.state.isActive === contact.id}/>
+    ))
+   }
+   return mapOverContacts(this.props.contacts)
+  }
 }
 
 class Contact extends Component {
+  handleClick = () => {
+    return this.props.onClick(this.props.useableKey)
+  }
   render() {
     return(
-      <p>Contact</p>
+      <div onClick={this.handleClick}>
+        <h2>{this.props.firstName} {this.props.lastName}</h2>
+        <p>{this.props.street}</p>
+        <p>{this.props.city}, {this.props.state}</p>
+        <p>{this.props.email}</p>
+      </div>
     )
   }
 }
